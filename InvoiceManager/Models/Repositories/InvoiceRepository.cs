6ee85ee8e04d3bb1bdb0154f8d5a1c0ed2b79d35
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-09-23 21:22:41 +0200

    LEKCJA 17: Kontroler.

---
+++ InvoiceManager/Models/Repositories/InvoiceRepository.cs
@@ -0,0 +1,64 @@
+﻿using InvoiceManager.Models.Domains;
+using System;
+using System.Collections.Generic;
+
+namespace InvoiceManager.Models.Repositories
+{
+    public class InvoiceRepository
+    {
+        public List<Invoice> GetInvoices(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Invoice GetInvoice(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MethodOfPayment> GetMethodsOfPayment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public InvoicePosition GetInvoicePosition(int invoicePositionId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPosition(InvoicePosition invoicePosition, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePosition(InvoicePosition invoicePosition, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UpdateInvoiceValue(int invoicePositionInvoiceId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePosition(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InvoiceManager/Models/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using InvoiceManager.Models.Domains;
+using System.Collections.Generic;
+
+namespace InvoiceManager.Models.Repositories
+{
+    public class ProductRepository
+    {
+        public List<Product> GetProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product GetProduct(int invoicePositionProductId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
