7e2d3ba62913432d57a17e3d26617c87c37f3a99
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-09-23 13:15:51 +0200

    LEKCJA 14: Uwierzytelnianie i Autoryzacja.

--- InvoiceManager/Controllers/HomeController.cs
+++ InvoiceManager/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace InvoiceManager.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,6 +10,7 @@ namespace InvoiceManager.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +18,7 @@ namespace InvoiceManager.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
